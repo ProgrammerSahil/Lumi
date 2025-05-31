7f5e86a9382d1fdcbdf7174e39c0e4752e89b5a5
Sahil Udar <udarsahil0@gmail.com>
2025-05-31 19:52:18 +0530

    added multiplication, division, power, variables

--- LVM/LVMMain.js
+++ LVM/LVMMain.js
@@ -1,51 +1,74 @@
-function LVMExec(bytecode){
-    const stack = [];
-    const memory = {};
-    let ip = 0;
+function LVMExec(bytecode) {
+  const stack = [];
+  const memory = {};
+  let ip = 0;
 
-    while(ip < bytecode.length){
-        const instr = bytecode[ip];
+  while (ip < bytecode.length) {
+    const instr = bytecode[ip];
 
-        switch(instr.op){
-            case "PUSH":
-                stack.push(instr.value);
-                break;
-            case "ADD":
-                let b = stack.pop();
-                let a = stack.pop();
-                stack.push(a+b);
-                break;
-            case "SUBTRACT":
-                let y = stack.pop();
-                let x = stack.pop();
-                stack.push(x-y);
-                break;
-            case "PRINT":
-                console.log(stack[stack.length-1]);
-                break;
-            case "HALT":
-                return;
-            default:
-                throw new Error(`Unknown Instruction: ${instr.op}`);
-                
+    switch (instr.op) {
+      case "PUSH":
+        stack.push(instr.value);
+        break;
+      case "ADD":
+        let b = stack.pop();
+        let a = stack.pop();
+        stack.push(a + b);
+        break;
+      case "SUBTRACT":
+        let y = stack.pop();
+        let x = stack.pop();
+        stack.push(x - y);
+        break;
+      case "MUL":
+        const m2 = stack.pop();
+        const m1 = stack.pop();
+        stack.push(m1 * m2);
+        break;
+
+      case "DIV":
+        const d2 = stack.pop();
+        const d1 = stack.pop();
+        if (d2 === 0) throw new Error("Divide by zero");
+        stack.push(d1 / d2);
+        break;
+
+      case "POW":
+        const exp = stack.pop();
+        const base = stack.pop();
+        stack.push(Math.pow(base, exp));
+        break;
+      case "PRINT":
+        console.log(stack[stack.length - 1]);
+        break;
+      case "STORE":
+        const value = stack.pop();
+        memory[bytecode.name] = value;
+        break;
+      case "LOAD":
+        if (!(bytecode.name in memory)) {
+          throw new Error(`Undeclared Variable: ${bytecode.value}`);
         }
-
-        ip++;
-
+        stack.push(memory[bytecode.name]);
+        break;
+      case "HALT":
+        return;
+      default:
+        throw new Error(`Unknown Instruction: ${instr.op}`);
     }
 
+    ip++;
+  }
 }
 
 const program = [
   { op: "PUSH", value: 10 },
   { op: "PUSH", value: 20 },
-  { op: "SUBTRACT" },
-  { op: "PUSH", value: 50},
-  { op: "ADD"},
+  { op: "MUL" },
+  { op: "STORE", name: "result" },
+  { op: "LOAD", value: "result" },
   { op: "PRINT" },
-  { op: "HALT" }
+  { op: "HALT" },
 ];
 
 LVMExec(program);
-
-

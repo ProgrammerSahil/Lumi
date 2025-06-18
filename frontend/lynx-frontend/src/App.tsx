8e198b5bbf31c63292a190ea7fa2f36f96f5ba36
Sahil Udar <udarsahil0@gmail.com>
2025-06-18 13:21:19 +0530

    added textarea

--- frontend/lynx-frontend/src/App.tsx
+++ frontend/lynx-frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 
 function App() {
+  
 
   return (
     <>
-      
+      <h1 className="compilerHeader">Compiler</h1>
+      <div className="mainContainer">
+        <textarea 
+          name="code" 
+          id="code" 
+          className="sourceCodeContainer"
+          rows={20}
+          cols={80}
+        />
+      </div>
     </>
   )
 }

3d2222468f5b488c12205accbcfd7a6408426b58
Sahil Udar <udarsahil0@gmail.com>
2025-06-18 17:44:44 +0530

    combined repetitive code

--- Compiler/bytecodeGenerator.js
+++ Compiler/bytecodeGenerator.js
@@ -1,105 +1,78 @@
 import { parseExpression } from "../Compiler/compiler.js";
 
+
+function generateBytecodeFromExpression(expression) {
+  const bytecode = [];
+
+  for (let token of expression) {
+    if (token.type === "NUMBER" || token.type === "STRING") {
+      bytecode.push({ op: "PUSH", value: token.value });
+    } else if (token.type === "IDENTIFIER") {
+      bytecode.push({ op: "LOAD", name: token.value });
+    } else if (token.type === "OPERATOR") {
+      switch (token.value) {
+        case "+":
+          bytecode.push({ op: "ADD" });
+          break;
+        case "-":
+          bytecode.push({ op: "SUBTRACT" });
+          break;
+        case "*":
+          bytecode.push({ op: "MUL" });
+          break;
+        case "/":
+          bytecode.push({ op: "DIV" });
+          break;
+        case "^":
+          bytecode.push({ op: "POW" });
+          break;
+        case "<":
+          bytecode.push({ op: "LESSTHAN" });
+          break;
+        case ">":
+          bytecode.push({ op: "GREATERTHAN" });
+          break;
+        case "==":
+          bytecode.push({ op: "EQUALS" });
+          break;
+        case "<=":
+          bytecode.push({ op: "LESSTHANEQUALS" });
+          break;
+        case ">=":
+          bytecode.push({ op: "GREATERTHANEQUALS" });
+          break;
+        case "!=":
+          bytecode.push({ op: "NOTEQUALS" });
+          break;
+        default:
+          throw new Error("Unsupported operator: " + token.value);
+      }
+    }
+  }
+
+  return bytecode;
+}
+
 function bytecodeGenerator(compiledCode) {
   let pointer = 0;
   const bytecodeOutput = [];
 
   while (pointer < compiledCode.length) {
-    if (compiledCode[pointer].type === "assignment") {
-      for (let exp of compiledCode[pointer].expression) {
-        if (exp.type === "NUMBER" || exp.type === "STRING") {
-          bytecodeOutput.push({ op: "PUSH", value: exp.value });
-        } else if (exp.type === "IDENTIFIER") {
-          bytecodeOutput.push({ op: "LOAD", name: exp.value });
-        } else if (exp.type === "OPERATOR") {
-          switch (exp.value) {
-            case "+":
-              bytecodeOutput.push({ op: "ADD" });
-              break;
-            case "-":
-              bytecodeOutput.push({ op: "SUBTRACT" });
-              break;
-            case "*":
-              bytecodeOutput.push({ op: "MUL" });
-              break;
-            case "/":
-              bytecodeOutput.push({ op: "DIV" });
-              break;
-            case "^":
-              bytecodeOutput.push({ op: "POW" });
-              break;
-            case "<":
-              bytecodeOutput.push({op: "LESSTHAN"});
-              break;
-            case ">":
-              bytecodeOutput.push({op: "GREATERTHAN"});
-              break;
-            case "==":
-              bytecodeOutput.push({op: "EQUALS"});
-              break;
-            case "<=":
-              bytecodeOutput.push({op: "LESSTHANEQUALS"});
-              break;
-            case ">=":
-              bytecodeOutput.push({op: "GREATERTHANEQUALS"});
-              break;
-            case "!=":
-              bytecodeOutput.push({op: "NOTEQUALS"});
-              break;
-          }
-        }
-      }
-      bytecodeOutput.push({
-        op: "STORE",
-        name: compiledCode[pointer].variableName,
-      });
-    } else if (compiledCode[pointer].type === "functionCall") {
-  const args = parseExpression(compiledCode[pointer].arguments);
-  for (let token of args) {
-    if (token.type === "IDENTIFIER") {
-      bytecodeOutput.push({ op: "LOAD", name: token.value });
-    } else if (token.type === "NUMBER" || token.type === "STRING") {
-      bytecodeOutput.push({ op: "PUSH", value: token.value });
-    } else if (token.type === "OPERATOR") {
-      switch (token.value) {
-        case "+":
-          bytecodeOutput.push({ op: "ADD" });
-          break;
-        case "-":
-          bytecodeOutput.push({ op: "SUBTRACT" });
-          break;
-        case "*":
-          bytecodeOutput.push({ op: "MUL" });
-          break;
-        case "/":
-          bytecodeOutput.push({ op: "DIV" });
-          break;
-        case "^":
-          bytecodeOutput.push({ op: "POW" });
-          break;
-        case "<":
-          bytecodeOutput.push({ op: "LESSTHAN" });
-          break;
-        case ">":
-          bytecodeOutput.push({ op: "GREATERTHAN" });
-          break;
-        case "==":
-          bytecodeOutput.push({ op: "EQUALS" });
-          break;
-        case "<=":
-          bytecodeOutput.push({ op: "LESSTHANEQUALS" });
-          break;
-        case ">=":
-          bytecodeOutput.push({ op: "GREATERTHANEQUALS" });
-          break;
-        case "!=":
-          bytecodeOutput.push({ op: "NOTEQUALS" });
-          break;
-      }
+    const node = compiledCode[pointer];
+
+    if (node.type === "assignment") {
+      const expressionBytecode = generateBytecodeFromExpression(node.expression);
+      bytecodeOutput.push(...expressionBytecode);
+      bytecodeOutput.push({ op: "STORE", name: node.variableName });
     }
-  }
-  bytecodeOutput.push({ op: "PRINT" });
-}
+
+    else if (node.type === "functionCall") {
+      const parsedArgs = parseExpression(node.arguments);
+      const argBytecode = generateBytecodeFromExpression(parsedArgs);
+      bytecodeOutput.push(...argBytecode);
+      bytecodeOutput.push({ op: "PRINT" });  // Replace with a lookup if you add more function types
+    }
+
     pointer++;
   }
 
